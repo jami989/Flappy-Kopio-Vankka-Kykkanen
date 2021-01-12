eb3355048fa3905ec253a83e37dc69e736c3e3f6
essikxamk <cesky001@edu.xamk.fi>
2021-01-12 17:35:08 +0200

    summary

---
+++ Flappy-Kopio-Vankka-Kykkanen/Assets/UnderwaterFantasy/Scripts/MoveBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private Vector2 startPos;
+    public float repeatWidth;
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);
+
+        if (transform.position.x < startPos.x - repeatWidth)
+          {
+            transform.position = startPos;
+          } 
+        
+    }
+}
