84237db35e9e894e7807d5faf30b2766f63e6b4b
jami989 <71631114+jami989@users.noreply.github.com>
2021-01-14 11:42:09 +0200

    added Obstacle manager

--- Flappy-Kopio-Vankka-Kykkanen/Assets/Scripts/Obstacle_generator.cs
+++ Flappy-Kopio-Vankka-Kykkanen/Assets/Scripts/Obstacle_generator.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Obstacle_generator : MonoBehaviour
 {
+    public float begin;
+    public float spawnintervals;
+
+    public GameObject Obstacle;
+
+    float position;
+
+    float starttimer;
+    float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,15 @@ public class Obstacle_generator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        starttimer = Time.realtimeSinceStartup;
+
+        if (starttimer > begin && starttimer > timer)
+        {
+            position = Random.Range(2f, 7.5f);
+            Instantiate(Obstacle, new Vector2(13, position), Quaternion.identity);
+            Instantiate(Obstacle, new Vector2(13, position - 8.5f), Quaternion.identity);
+
+            timer = Time.realtimeSinceStartup + spawnintervals;
+        }
     }
 }
